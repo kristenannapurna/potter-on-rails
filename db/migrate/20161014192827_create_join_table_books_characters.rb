be9e792bb527539a079f66382029c8ea615374e1
Kristen Spencer <kristen@kristenspencer.ca>
2016-10-14 19:41:22 -0400

    where i left off with the join table

--- db/migrate/20161014192827_create_join_table_books_characters.rb
+++ db/migrate/20161014192827_create_join_table_books_characters.rb
@@ -1,4 +1,10 @@
 class CreateJoinTableBooksCharacters < ActiveRecord::Migration
   def change
+  	create_table :books_characters, id: false do |t|
+  		t.integer :book_id
+  		t.integer :character_id
   end
 end
+end
+
+
